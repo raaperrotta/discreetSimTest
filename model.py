ce48b11eed200510d2cb1ed9d8b2173fc9d2b051
Robert Perrotta <raaperrotta@gmail.com>
2015-03-06 21:25:09 -0500

    most of the way to a basic implementation

--- model.py
+++ model.py
@@ -12,82 +12,107 @@ class Uav(object):
         self.speed = speed
         self.lethality = lethality
         self.target = target
-        self.aggressors = []
-
-        self.action = env.process(self.blow_up_ship())
+                
+        # Still need to correct for non-zero ship speed
+        time_to_ship = self.range_to_target(0)/self.speed
+        
+        self.reached_ship = env.timeout(time_to_ship)
+        self.shot_down = env.event();
+        
+        self.action = env.process(self.attack())
 
     def __str__(self):
         return "Uav_" + str(id(self))
-
-    def blow_up_ship(self):
-            try:
-                dist_to_ship = n.linalg.norm(self.pos-self.target.pos)
-                # Still need to correct for non-zero ship speed
-                yield self.env.timeout(dist_to_ship/self.speed)
-                print "%s hit %s at %.2f" % (self, self.target, self.env.now)
-                # self.env.exit(1)
-            except simpy.Interrupt:
-                # if the UAV is shot down we can cancel the ship impact
-                # everyone can stop shooting at this uav now
-                for weapon in self.aggressors:
-                    weapon.action.interrupt()
-
+    
+    def range_to_target(self,time):
+        dist_to_ship = n.linalg.norm(self.pos-self.target.pos)
+        return dist_to_ship - time*self.speed
+        
+    def attack(self):
+        result = yield self.reached_ship | self.shot_down
+        if self.reached_ship in result:
+            print "%s hit %s at %.2f" % (self, self.target, self.env.now)
+        else: # self.shot_down in result
+            print "%s was shot down at %.2f" % (self, self.env.now)
+            
+            
 class Weapon(object):
-    def __init__(self, env, rate, lethality):
-        # self = simpy.PriorityResource(env, capacity=1)
+    def __init__(self, env, rate, lethality, ship):
         self.env = env
         self.rate = rate
         self.lethality = lethality
+        self.resource = simpy.Resource(env, capacity=1)
+        self.ship = ship
+        ship.weapons.append(self)
+        
+        self.destroyed = env.event()
 
     def __str__(self):
         return "Wpn_" + str(id(self))
 
     def shoot_uav(self,uav):
-        uav.aggressors.append(self)
         period = 1./self.rate
         time_to_kill = period
         while random.random() > self.lethality:
             time_to_kill += period
-        try:
-            yield self.env.timeout(time_to_kill)
-            uav.aggressors.remove(self)
-            uav.action.interrupt()
-            print "%s shot-down %s at %.2f" % (self, uav, self.env.now)
-        except simpy.Interrupt:
+
+        print "%s engaged %s at %.2f" % (self, uav, self.env.now)
+        result = yield self.env.timeout(time_to_kill) | uav.reached_ship
+        if uav.reached_ship in result:
             pass
+            # the UAV object will handle this case
+        else:
+            uav.shot_down.succeed()
+            print "%s shot-down %s at %.2f" % (self, uav, self.env.now)
 
 
 class Radar(object):
-    def __init__(self, env, period, ranges, p_detect):
+    def __init__(self, env, period, ranges, p_detect, ship, uavs):
         self.env = env
         self.period = period
         self.ranges = ranges
         self.p_detect = p_detect
+        self.ship = ship
+        self.uavs = uavs
+        ship.radars.append(self)
+        
+        self.destroyed = env.event()
+        
+        self.action = []
+        for uav in self.uavs:
+            self.action.append(env.process(self.search(uav)))
 
     def __str__(self):
         return "Rdr_" + str(id(self))
-
-        """
-        self.action = env.process(self.search())
-
-    def search(self):
-        pass
-"""
-
+    
+    def search(self,uav):
+        time_to_detect = 0
+        detected_uav = self.env.timeout(time_to_detect)
+        result = yield self.destroyed | uav.reached_ship | detected_uav
+        if detected_uav in result:
+            print "%s spotted %s at %.2f" % (self, uav, self.env.now)
+            self.env.process(self.ship.alert(uav))
+            
 class Ship(object):
-    def __init__(self, env, pos, speed, weapons, radars):
+    def __init__(self, env, pos, speed):
         self.env = env
         self.pos = pos
         self.speed = speed
-        self.weapons = weapons
-        self.radars = radars
+        self.weapons = []
+        self.radars = []
 
     def __str__(self):
         return "Shp_" + str(id(self))
-
-class World(object):
-    def __init__(self,weapons,radars,ships,uavs): # will add weather later
-        self.weapons = weapons
-        self.radars = radars
-        self.ships = ships
-        self.uavs = uavs
+    
+    def alert(self, uav):
+        print "%s is aware of %s at %.2f" % (self, uav, self.env.now)
+        events = [uav.reached_ship, uav.shot_down]
+        for weapon in self.weapons:
+            events.append(weapon.resource.request)
+            
+        result = yield simpy.events.AnyOf(self.env, events)
+        if uav.reached_ship in result or uav.shot_down in result:
+            print "hi"
+        else:
+            for weapon in result:
+                print weapon
--- sim.py
+++ sim.py
@@ -5,34 +5,42 @@ from model import *
 def iterate():
     env = simpy.Environment()
 
+    ships = []
     weapons = []
     radars = []
-    ships = []
     uavs = []
 
     max_detection_range = 60.
 
-    weapons.append(Weapon(env, rate=10., lethality=0.01))
-    weapons.append(Weapon(env, rate=0.25, lethality=0.2))
-
-    radars.append(Radar(env, period=2., ranges=[max_detection_range, 20.], p_detect=n.array([0.2, 0.8])))
-
-    ships.append(Ship(env, pos=n.array([0.,0.]), speed=0., weapons=weapons, radars=radars))
-
+    ships.append(Ship(env, pos=n.array([0.,0.]), speed=0.))
+    
+    # Might want a generator function later
+    # At least should have a way to delay/stagger the UAV creation
     uavs.append(Uav(env, pos=n.array([100.,0.]), speed=10, lethality=0.8, target=ships[0]))
     uavs.append(Uav(env, pos=n.array([110.,0.]), speed=10, lethality=0.8, target=ships[0]))
 
-    weapons[0].action = env.process(weapons[0].shoot_uav(uavs[0]))
-    weapons[1].action = env.process(weapons[1].shoot_uav(uavs[1]))
+    radars.append(Radar(env, period=2., ranges=[max_detection_range, 20.], p_detect=n.array([0.2, 0.8]), ship=ships[0], uavs=uavs))
+    
+    weapons.append(Weapon(env, rate=10., lethality=0.01, ship=ships[0]))
+    weapons.append(Weapon(env, rate=0.25, lethality=0.2, ship=ships[0]))
 
     print " "
+    print ships
     print uavs
-    print "%s, " % (uav for uav in uavs)
+    print radars
     print weapons
+    print " "
 
+    # weapons[0].action = env.process(weapons[0].shoot_uav(uavs[0]))
+    # weapons[1].action = env.process(weapons[1].shoot_uav(uavs[1]))
+    
     env.run()
     return 0
 
+
+iterate()
+
+"""
 # The iterator that calls each iteration and handles the results
 num_runs = 10.
 num_fail = 0
@@ -41,3 +49,4 @@ for ii in range(10):
 
 risk = num_fail/num_runs
 print "Hit " + str(100.*risk) + "% of the time."
+"""
